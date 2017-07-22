484d1b1bc71ea5f4688a2199f70c1f4c074f8a41
Khue Vu <khuevu@gmail.com>
2017-07-22 09:11:19 +0100

    Add test

--- server/test/bt_consentbox.t.cpp
+++ server/test/bt_consentbox.t.cpp
@@ -25,8 +25,37 @@ TEST(ConsentBox, TwoConsentVotes_InOrder)
     auto msgId = consentBox.getNextConsensus(msg);
 
     EXPECT_EQ(MsgTypeTankAction, msgId);
-//    auto mta = ;
     EXPECT_EQ(tankAction, *(MsgTankAction*) msg);
     EXPECT_EQ(tankAction2, *(MsgTankAction*) msg);
 
+}
+
+
+TEST(ConsentBox, TwoConsentVotes_OutOfOrder)
+{
+
+    ConsentBox consentBox;
+
+    MsgTankAction tankAction;
+    tankAction.action = Tank::EXPLODE;
+    tankAction.tankId = 15;
+    consentBox.vote(0, MsgTypeTankAction, (const char*) &tankAction, sizeof(tankAction));
+
+    MsgTankAction differentAction;
+    differentAction.action = Tank::ITEM;
+    differentAction.tankId = 20;
+    consentBox.vote(1, MsgTypeTankAction, (const char*) &differentAction, sizeof(differentAction));
+
+    MsgTankAction tankAction2;
+    tankAction2.action = Tank::EXPLODE;
+    tankAction2.tankId = 15;
+    consentBox.vote(1, MsgTypeTankAction, (const char*) &tankAction2, sizeof(tankAction2));
+
+    char msg[1024];
+    auto msgId = consentBox.getNextConsensus(msg);
+
+    EXPECT_EQ(MsgTypeTankAction, msgId);
+    EXPECT_EQ(tankAction, *(MsgTankAction*) msg);
+    EXPECT_EQ(tankAction2, *(MsgTankAction*) msg);
+    EXPECT_FALSE(differentAction == *(MsgTankAction*) msg);
 }
